b5e8b6db810597e635bad49592fd3731b9174b25
Dayana <dayileyvas@gmail.com>
2025-07-14 14:03:23 -0500

    first settings, adding 3 todos

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -21,9 +22,20 @@ export default defineConfig([
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      plugins: {
+        react,
+      },
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off',
     },
   },
 ])
