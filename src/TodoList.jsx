f96cc6604b1b7a830a6233d5bd2d938db337c6e3
Dayana <dayileyvas@gmail.com>
2025-08-10 11:09:20 -0500

    Todolist updated with ternary op

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,21 +3,19 @@ import TodoListItem from './TodoListItem';
 function TodoList({ todoList = [], onCompleteTodo }) {
   const filteredTodoList = todoList.filter((t) => !t.isCompleted);
 
-  if (filteredTodoList.length === 0) {
-    return <p>Add todo above to get started</p>;
-  } else {
-    return (
-      <ul>
-        {filteredTodoList.map((todo) => (
-          <TodoListItem
-            key={todo.id}
-            todo={todo}
-            onCompleteTodo={onCompleteTodo}
-          />
-        ))}
-      </ul>
-    );
-  }
+  return filteredTodoList.length === 0 ? (
+    <p>Add todo above to get started</p>
+  ) : (
+    <ul>
+      {filteredTodoList.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          onCompleteTodo={onCompleteTodo}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
