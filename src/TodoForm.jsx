425cc707847f48cd4f0fa0a469d392a88fad45e4
Dayana <dayileyvas@gmail.com>
2025-08-10 11:01:46 -0500

    week 5 Conditional, controlled rendering

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,29 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function TodoForm({ onAddTodo }) {
+  const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   const todoTitleInput = useRef();
 
   function handleAddTodo(event) {
     event.preventDefault();
-    const title = event.target.title.value;
-    onAddTodo(title);
-    event.target.title.value = '';
-    todoTitleInput.current.focus();
+    onAddTodo(workingTodoTitle);
+    setWorkingTodoTitle('');
   }
 
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" name="title" ref={todoTitleInput} />
-      <button>Add Todo</button>
+      <input
+        type="text"
+        value={workingTodoTitle}
+        onChange={(e) => setWorkingTodoTitle(e.target.value)}
+        id="todoTitle"
+        name="title"
+        ref={todoTitleInput}
+      />
+      <button type="submit" disabled={workingTodoTitle === ''}>
+        Add Todo
+      </button>
     </form>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,23 @@
 import TodoListItem from './TodoListItem';
 
-function TodoList({ todoList }) {
-  return (
-    <ul>
-      {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
-  );
+function TodoList({ todoList = [], onCompleteTodo }) {
+  const filteredTodoList = todoList.filter((t) => !t.isCompleted);
+
+  if (filteredTodoList.length === 0) {
+    return <p>Add todo above to get started</p>;
+  } else {
+    return (
+      <ul>
+        {filteredTodoList.map((todo) => (
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onCompleteTodo={onCompleteTodo}
+          />
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default TodoList;
