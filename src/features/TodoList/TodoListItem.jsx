e5589bc843d25af691613dd0337760d7859200bc
Dayana <dayileyvas@gmail.com>
2025-08-27 01:07:42 -0500

    useEffect todolistItem

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TextInputWithLabel from '../../shared/TextInputWithLabel';
 
 function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
